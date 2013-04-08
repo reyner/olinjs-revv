3c6738769ad28b186893cb6acd7fcb837e3655e3
Juliana Nazaré <jules.nazare@gmail.com>
2013-04-08 00:31:26 -0400

    reset

--- routes/sms.js
+++ routes/sms.js
@@ -1,4 +1,6 @@
+var twilio = require('twilio');
+var client = new twilio.RestClient('ACdd2af7b3991b85c37ef50d5aba5894a6', '0b36a23e971506e25b78f6912446d713');
+
 exports.load = function(req, res){
-	var client = new twilio.RestClient('ACdd2af7b3991b85c37ef50d5aba5894a6', '0b36a23e971506e25b78f6912446d713');
   	res.render('index', { title: 'Express' });
 };
