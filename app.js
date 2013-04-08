635f41ded7172c364f9f0ec85f23248d03c43e82
Juliana Nazaré <jules.nazare@gmail.com>
2013-04-08 00:12:05 -0400

    remove twilio

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
-  , twilio = require('twilio')
   , twil = require('./routes/twil')
   , path = require('path');
 
