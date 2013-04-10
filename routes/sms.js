a67213d9d1792140b8862cf31b9a65b9823c0745
Juliana Nazaré <jules.nazare@gmail.com>
2013-04-10 17:53:35 -0400

    it flipping works

--- routes/sms.js
+++ routes/sms.js
@@ -2,7 +2,7 @@ var twilio = require('twilio');
 var client = new twilio.RestClient(process.env.TWILIO_KEY, process.env.TWILIO_SECRET);
 
 exports.load = function(req, res){
-    console.log(req.body);
+    console.log(req.body.Body);
     
     client.sms.messages.create({
         to:req.body.From,
