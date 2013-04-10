35eb5dbe60dd35ceba873cb26699d5892fa4469f
Juliana Nazaré <jules.nazare@gmail.com>
2013-04-10 17:46:22 -0400

    this miiiight work

--- routes/sms.js
+++ routes/sms.js
@@ -3,5 +3,24 @@ var client = new twilio.RestClient(process.env.TWILIO_KEY, process.env.TWILIO_SE
 
 exports.load = function(req, res){
     console.log(req.body);
-    res.send(req.body);
+    
+    client.sms.messages.create({
+        to:req.session.user.phone_number,
+        from:'+14076245704',
+        body:'Your email has been sent.'
+    }, function(error, message) {
+        
+        if (!error) {
+        
+            console.log('Success! The SID for this SMS message is:');
+            console.log(message.sid);
+     
+            console.log('Message sent on:');
+            console.log(message.dateCreated);
+        }
+        else {
+            console.log('Oops! There was an error.');
+            // console.log(error);
+        }
+    });
 };
