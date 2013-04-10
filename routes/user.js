5a5d337a51221166b6fafd63c0270aa77aa145af
Juliana Nazaré <jules.nazare@gmail.com>
2013-04-10 00:45:51 -0400

    frontend is purty

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,7 @@
 var models = require('../models');
 var User = models.User;
+var twilio = require('twilio');
+var client = new twilio.RestClient(process.env.TWILIO_KEY, process.env.TWILIO_SECRET);
 
 exports.new = function(req, res){
 	console.log(req.body);
@@ -10,12 +12,41 @@ exports.new = function(req, res){
 		email_password: req.body.email_password
 	});
 
+	req.session.user = user;
+
 	user.save(function (err){
 		if (err){
 			return console.log("error", err);
 		}
 		else{
-			res.send("Your new user \n" + req.body.phone_number + " has been added!");
+			client.sms.messages.create({
+			    to:user.phone_number,
+			    from:'+14076245704',
+			    body:'Welcome to MailMS'
+			}, function(error, message) {
+			    
+			    // The HTTP request to Twilio will run asynchronously.  This callback
+			    // function will be called when a response is received from Twilio
+			    
+			    // The "error" variable will contain error information, if any.
+			    // If the request was successful, this value will be "falsy"
+			    if (!error) {
+			        
+			        // The second argument to the callback will contain the information
+			        // sent back by Twilio for the request.  In this case, it is the
+			        // information about the text messsage you just sent:
+			        console.log('Success! The SID for this SMS message is:');
+			        console.log(message.sid);
+			 
+			        console.log('Message sent on:');
+			        console.log(message.dateCreated);
+			    }
+			    else {
+			        console.log('Oops! There was an error.');
+			        // console.log(error);
+			    }
+			});
+			res.render('user');
 		}
 	});
 };
