80ad67cbd1ef2994460d954a4c4140f2b6b701f6
Juliana Nazaré <jules.nazare@gmail.com>
2013-04-10 17:42:51 -0400

    test

--- routes/sms.js
+++ routes/sms.js
@@ -2,5 +2,6 @@ var twilio = require('twilio');
 var client = new twilio.RestClient(process.env.TWILIO_KEY, process.env.TWILIO_SECRET);
 
 exports.load = function(req, res){
+    console.log(req.body);
     res.send(req.body);
 };
