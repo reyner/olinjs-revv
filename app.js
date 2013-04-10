ff54d8a4c8924710245f5291be1c6cd27ff4d2a5
Juliana Nazaré <jules.nazare@gmail.com>
2013-04-10 17:10:34 -0400

    blah cannot respond

--- app.js
+++ app.js
@@ -39,15 +39,6 @@ app.get('/sms', sms.load);
 
 app.post('/users', user.new);
 
-var sys = require('sys');
-app.post('/incoming', function(req, res) {
-  var message = req.body.Body;
-  var from = req.body.From;
-  sys.log('From: ' + from + ', Message: ' + message);
-    var twiml = '<?xml version="1.0" encoding="UTF-8" ?>n<Response>n<Sms>Thanks for your text, well be in touch.</Sms>n</Response>';
-    res.send(twiml, {'Content-Type':'text/xml'}, 200);
-});
-
 imap.openInbox(function () {
   http.createServer(app).listen(app.get('port'), function(){
     console.log("Express server listening on port " + app.get('port'));
