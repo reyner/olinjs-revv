357e0d88577ec424a64050285b68fdcb64c12a86
Juliana Nazaré <jules.nazare@gmail.com>
2013-04-10 13:56:19 -0400

    trying to get repsonse

--- routes/sms.js
+++ routes/sms.js
@@ -3,21 +3,12 @@ var client = new twilio.RestClient(process.env.TWILIO_KEY, process.env.TWILIO_SE
 
 exports.load = function(req, res){
     client.sms.messages.create({
-        to:'+17038017814',
+        to:req.session.user.phone_number,
         from:'+14076245704',
         body:'This is where your email text goes'
     }, function(error, message) {
-        
-        // The HTTP request to Twilio will run asynchronously.  This callback
-        // function will be called when a response is received from Twilio
-        
-        // The "error" variable will contain error information, if any.
-        // If the request was successful, this value will be "falsy"
         if (!error) {
             
-            // The second argument to the callback will contain the information
-            // sent back by Twilio for the request.  In this case, it is the
-            // information about the text messsage you just sent:
             console.log('Success! The SID for this SMS message is:');
             console.log(message.sid);
      
